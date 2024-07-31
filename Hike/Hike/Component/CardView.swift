7a6ddbe1bb520f4fd9ae2bbe9d35189bc920e3d2
Sahil Naik <nsahil992@gmail.com>
2024-07-31 21:18:14 +0530

    feat: cp

--- Hike/Hike/Component/CardView.swift
+++ Hike/Hike/Component/CardView.swift
@@ -43,6 +43,20 @@ struct CardView: View {
                         .resizable()
                         .scaledToFit()
                 }
+                
+                Button {
+                    print("The button was pressed.")
+                } label: {
+                    Text("Explore more")
+                        .font(.title2)
+                        .fontWeight(.heavy)
+                        .foregroundStyle(LinearGradient(colors: [.customGreenLight, .customGreenMedium], startPoint: .top, endPoint: .bottom))
+                        .shadow(color: .black.opacity(0.25), radius: 0.25, x: 1, y: 2)
+                }
+                .padding(.vertical)
+                .padding(.horizontal, 30)
+                .background(LinearGradient(colors: [.customGrayLight, .customGrayMedium], startPoint: .top, endPoint: .bottom))
+                .cornerRadius(40)
             }
         }
         .frame(width: 320, height: 570)
